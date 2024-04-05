be3d8a3eaa64f19bc45b60daa6c5cd993231686c
Franco Moravcik <francomoravcik12.fm@gmail.com>
2024-04-05 15:19:38 -0300

    cambio de ruta

--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 import Photosnap from './components/photosnap/Photosnap';
-import Features from './components/features/Features'
-import Stories from './components/stories/Stories'
-import Pricing from './components/pricing/Pricing'
+import Features from './components/features/Features';
+import Stories from './components/stories/Stories';
+import Pricing from './components/pricing/Pricing';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-
-     <BrowserRouter>
-
-     <Routes>
-      <Route exact path='/' element={<Photosnap/>}></Route>
-      <Route exact path='/stories' element={<Stories/>}></Route>
-      <Route exact path='/features' element={<Features/>}></Route>
-      <Route exact path='/pricing' element={<Pricing/>}></Route>
-
-     </Routes>
-
-     </BrowserRouter>
-
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Photosnap />} />
+        <Route exact path='/stories' element={<Stories />} />
+        <Route exact path='/features' element={<Features />} />
+        <Route exact path='/pricing' element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
